616a17c8dd23b177b97cf60ed1bafd03f5cb1a00
leifeggenfellner <eggenfellner@protonmail.com>
2022-11-03 20:20:02 +0100

    cleanup

--- build/image_preload.cmake
+++
@@ -1,5 +0,0 @@
-# Generated file that can be used to preload variant images
-set(CACHED_CONF_FILE "/Users/leif/Code/src/github/leifeggenfellner/nordic-orbit-course/remote_controller/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "/Users/leif/Code/src/github/leifeggenfellner/nordic-orbit-course/remote_controller/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/opt/nordic/ncs/toolchains/v2.1.1/bin/python3" CACHE INTERNAL "NCS child image controlled")
